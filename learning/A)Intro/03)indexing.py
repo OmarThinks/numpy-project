9b9633ea6b58d31c199884b0ee5406d45b518b65
Omar Magdy <contact.parmag@gmail.com>
2021-07-23 20:27:34 +0200

    indexing.py:add:add examples

---
+++ learning/A)Intro/03)indexing.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+array = np.array([[1,2,3],[4,5,6]])
+print(array)
+print(array.ndim)
+"""
+[[1 2 3]
+ [4 5 6]]
+2
+"""
+
+print(array[0])
+"""
+[1,2,3]
+"""
+print(array[0][2])
+"""
+3
+"""
+
+
+
+
