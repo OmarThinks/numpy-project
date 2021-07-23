d9c98b3b58850727dd011bce3db90512bd9e859a
Omar Magdy <contact.parmag@gmail.com>
2021-07-24 01:38:30 +0200

    slicing:mod

--- learning/A)Intro/05_slicing.py
+++ learning/A)Intro/05_slicing.py
@@ -1,3 +1,31 @@
 import numpy as np
 
 
+
+arr0 = np.array([0,1,2,3,4,5,6,7,8])
+
+print(arr0[1:5]) # from index 1 to index 5
+# [1 2 3 4]
+print(arr0[1:5:2]) # from index 1 to index 5 with step 2
+# [1 3]
+print(arr0[:5]) # from start to index 5 with step 2
+# [0 1 2 3 4]
+print(arr0[::3]) # from start to end with step 3
+# [0 3 6]
+
+"""Negative Slicing"""
+
+print(arr0[-2:]) # last 2 elements??????
+# [7 8]
+
+
+
+
+print("")
+print("2D Slicing")
+
+
+arr_2 = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+
+print(arr_2[0:1,1:4])
+# [[2 3 4]]
