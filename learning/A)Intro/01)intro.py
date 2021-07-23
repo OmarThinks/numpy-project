02a46aa201c826515b4d7b3e50471d818af3587b
Omar Magdy <contact.parmag@gmail.com>
2021-07-23 18:56:55 +0200

    01 intro:add: examples

---
+++ learning/A)Intro/01)intro.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+print("arr1")
+arr1 = np.array([1, 2, 3, 4, 5])
+print(arr1)
+print(type(arr1))
+
+
+
+print("___")
+print("arr2")
+arr2 = np.array(range(1,6))
+print(arr2)
+print(type(arr2))
+
+
+
