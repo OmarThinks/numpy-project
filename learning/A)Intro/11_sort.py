6e5abbc09abd0e49152c05731e37c728cbb57ae1
Omar Magdy <contact.parmag@gmail.com>
2021-07-24 08:47:03 +0200

    sort.py:add:examples

---
+++ learning/A)Intro/11_sort.py
@@ -0,0 +1,35 @@
+import numpy as np 
+
+
+array = np.array([5, 9, 4, 4, 7, 6, 3])
+
+newarray = np.sort(array)
+print(newarray)
+# [3 4 4 5 6 7 9]
+
+
+
+array = np.array(["cars","apple","bannana"])
+newarray = np.sort(array)
+print(newarray)
+# ['apple' 'bannana' 'cars']
+
+
+
+
+
+array = np.array([[9,5,7,3],[1,6,2,7]])
+newarray = np.sort(array)
+print(newarray)
+# [[3 5 7 9]
+# [1 2 6 7]]
+
+
+
+
+
+
+
+
+
+
